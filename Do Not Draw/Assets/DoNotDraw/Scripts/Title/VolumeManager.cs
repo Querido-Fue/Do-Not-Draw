acd92169ecbd77757a475f62ca824922465d43e2
redCaramel <parkjunesung0912@gmail.com>
2026-08-19 13:19:20 +0900

    [Feat] VolumeManager 추가

--- Do Not Draw/Assets/DoNotDraw/Scripts/Title/VolumeManager.cs
+++ Do Not Draw/Assets/DoNotDraw/Scripts/Title/VolumeManager.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class VolumeManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private static VolumeManager instance = null;
+
+    void Awake()
     {
-        
+        if (null == instance)
+        {
+            instance = this;
+            this.bgmVolume = 1f;
+            this.sfxVolume = 1f;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static VolumeManager Instance
     {
-        
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
     }
+
+    public float bgmVolume;
+    public float sfxVolume;
 }
+
+// 전체 bgm, sfx 볼륨 저장 클래스
